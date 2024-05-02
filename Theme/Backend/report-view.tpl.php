d1ff07bad37e2a7f468e5d02f449374b241905a1
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-05-02 22:54:39 +0000

    Went through todos

--- Theme/Backend/report-view.tpl.php
+++ Theme/Backend/report-view.tpl.php
@@ -142,7 +142,7 @@ echo $this->data['nav']->render(); ?>
 
         <div id="elements">
             <template id="elementTpl">
-                <section id="taskelmenet-0" class="portlet taskElement"
+                <section id="taskelement-0" class="portlet taskElement"
                     data-update-content="#elements"
                     data-update-element=".taskElement .taskElement-content"
                     data-update-tpl="#taskElementContentTpl"
@@ -229,7 +229,7 @@ echo $this->data['nav']->render(); ?>
                 <?php endif; ?>
 
                 <?php if ($element->description !== '') : ?>
-                <section id="taskelmenet-<?= $element->id; ?>" class="portlet taskElement"
+                <section id="taskelement-<?= $element->id; ?>" class="portlet taskElement"
                     data-update-content="#elements"
                     data-update-element=".taskElement .taskElement-content"
                     data-update-tpl="#taskElementContentTpl"
